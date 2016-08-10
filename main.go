e2c8e7d9129774c930a48d8ff6a847e7a102a480
fouralarmfire <claudiaberesford@googlemail.com>
2016-08-10 17:22:15 +0100

    colourful groot

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/fouralarmfire/grootsay/ascii"
 	"os"
 	"strings"
@@ -21,7 +22,7 @@ func processStdin() bool {
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
 		stdinScanner := bufio.NewScanner(os.Stdin)
 		for stdinScanner.Scan() {
-			fmt.Println(stdinScanner.Text())
+			fmt.Println(color.MagentaString(stdinScanner.Text()))
 		}
 		return true
 	}
@@ -31,7 +32,7 @@ func processStdin() bool {
 func processArgs() bool {
 	args := os.Args[1:]
 	if len(args) > 0 {
-		fmt.Printf("         %s\n", strings.Trim(fmt.Sprint(args), "[]"))
+		fmt.Printf(color.CyanString("         %s\n", strings.Trim(fmt.Sprint(args), "[]")))
 		return true
 	}
 	return false
