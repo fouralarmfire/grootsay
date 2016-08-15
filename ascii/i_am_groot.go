d117aa8afb61ed5d61bb9e0a5a0fc82dc4221d95
fouralarmfire <claudiaberesford@googlemail.com>
2016-08-15 21:12:13 +0100

    Separate colours into own package

--- ascii/i_am_groot.go
+++ ascii/i_am_groot.go
@@ -1,68 +1,38 @@
 package ascii
 
 import (
-	"github.com/fatih/color"
+	"github.com/fouralarmfire/grootsay/colours"
 )
 
 func CreateGroot() {
-	printRed("                 \\           ,=      Z8ZO")
-	printRed("                  \\          7=  D  77$7$Z:  OZZ")
-	printRed("                     O8      Z    $?$ D88Z, Z$Z,")
-	printCyan("                    8D$     $+     ZO  IID$?7$ND,")
-	printCyan("                     +OI=  ?$I?   78?~7IIMO?I8Z")
-	printCyan("                       ZO87II$??O$7I???I???IIO")
-	printBlue("                        O$77I$??I7II$I?I?O?II")
-	printBlue("                         Z$$7O7$??????I7$III7")
-	printMagenta("                         $7ZMMNM$$???$MMMNZ$7")
-	printMagenta("                         ZZMMMMMM7I??MMMMMM7$")
-	printMagenta("                         $77NMNM$7???7MMNM&Z$")
-	printBlue("                          77I??$7I$II?7$I?I7")
-	printBlue("                           ~$7$III87?II77$,")
-	printGreen("                                 `NDO'        8")
-	printGreen("                               OO$ZZZ78I    $ZO")
-	printGreen("                            8IZO8$7?7O?IIOZOO")
-	printGreen("                          $O$    .II?.  'O")
-	printGreen("                         $'       ZID")
-	printGreen("                                 ,7Z$,")
-	printYellow("                            +8NMO7ZO77DZI:")
-	printYellow("                           ++?$8OOO8N8DMN7+")
-	printYellow("                           ?+++=~~====+=++?")
-	printYellow("                           ??++=~~=++++++??")
-	printYellow("                           ???+=~~=++++++??")
+	colours.PrintRed("                 \\           ,=      Z8ZO")
+	colours.PrintRed("                  \\          7=  D  77$7$Z:  OZZ")
+	colours.PrintRed("                     O8      Z    $?$ D88Z, Z$Z,")
+	colours.PrintCyan("                    8D$     $+     ZO  IID$?7$ND,")
+	colours.PrintCyan("                     +OI=  ?$I?   78?~7IIMO?I8Z")
+	colours.PrintCyan("                       ZO87II$??O$7I???I???IIO")
+	colours.PrintBlue("                        O$77I$??I7II$I?I?O?II")
+	colours.PrintBlue("                         Z$$7O7$??????I7$III7")
+	colours.PrintMagenta("                         $7ZMMNM$$???$MMMNZ$7")
+	colours.PrintMagenta("                         ZZMMMMMM7I??MMMMMM7$")
+	colours.PrintMagenta("                         $77NMNM$7???7MMNM&Z$")
+	colours.PrintBlue("                          77I??$7I$II?7$I?I7")
+	colours.PrintBlue("                           ~$7$III87?II77$,")
+	colours.PrintGreen("                                 `NDO'        8")
+	colours.PrintGreen("                               OO$ZZZ78I    $ZO")
+	colours.PrintGreen("                            8IZO8$7?7O?IIOZOO")
+	colours.PrintGreen("                          $O$    .II?.  'O")
+	colours.PrintGreen("                         $'       ZID")
+	colours.PrintGreen("                                 ,7Z$,")
+	colours.PrintYellow("                            +8NMO7ZO77DZI:")
+	colours.PrintYellow("                           ++?$8OOO8N8DMN7+")
+	colours.PrintYellow("                           ?+++=~~====+=++?")
+	colours.PrintYellow("                           ??++=~~=++++++??")
+	colours.PrintYellow("                           ???+=~~=++++++??")
 }
 
 func SayIAmGroot() {
-	printMagenta("        ____________")
-	printCyan("      <  I AM GROOT  >")
-	printMagenta("        ------------")
-}
-
-func printRed(str string) {
-	red := color.New(color.FgRed).PrintlnFunc()
-	red(str)
-}
-
-func printGreen(str string) {
-	green := color.New(color.FgGreen).PrintlnFunc()
-	green(str)
-}
-
-func printYellow(str string) {
-	yellow := color.New(color.FgYellow).PrintlnFunc()
-	yellow(str)
-}
-
-func printBlue(str string) {
-	blue := color.New(color.FgBlue).PrintlnFunc()
-	blue(str)
-}
-
-func printCyan(str string) {
-	cyan := color.New(color.FgCyan).PrintlnFunc()
-	cyan(str)
-}
-
-func printMagenta(str string) {
-	magenta := color.New(color.FgMagenta).PrintlnFunc()
-	magenta(str)
+	colours.PrintMagenta("        ____________")
+	colours.PrintCyan("      <  I AM GROOT  >")
+	colours.PrintMagenta("        ------------")
 }
---
+++ colours/colours.go
@@ -0,0 +1,45 @@
+package colours
+
+import (
+	"github.com/fatih/color"
+)
+
+func PrintRed(str string) {
+	red := color.New(color.FgRed).PrintlnFunc()
+	red(str)
+}
+
+func PrintGreen(str string) {
+	green := color.New(color.FgGreen).PrintlnFunc()
+	green(str)
+}
+
+func PrintYellow(str string) {
+	yellow := color.New(color.FgYellow).PrintlnFunc()
+	yellow(str)
+}
+
+func PrintBlue(str string) {
+	blue := color.New(color.FgBlue).PrintlnFunc()
+	blue(str)
+}
+
+func PrintCyan(str string) {
+	cyan := color.New(color.FgCyan).PrintlnFunc()
+	cyan(str)
+}
+
+func PrintMagenta(str string) {
+	magenta := color.New(color.FgMagenta).PrintlnFunc()
+	magenta(str)
+}
+
+func PrintFCyan(str string, thing []string) {
+	cyan := color.New(color.FgCyan).PrintfFunc()
+	cyan(str, thing)
+}
+
+func PrintFMagenta(str string, thing []string) {
+	magenta := color.New(color.FgMagenta).PrintfFunc()
+	magenta(str, thing)
+}
