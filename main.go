68c0263b1ddd8a2ca2da1f1fb24e72c39fe69a6e
fouralarmfire <claudiaberesford@googlemail.com>
2016-08-15 20:58:03 +0100

    Form multi-line bubbles for stdin
    sortof

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/fouralarmfire/grootsay/ascii"
 )
 
+const maxLineLength = 80
+
 func main() {
 	fmt.Print("\n")
 	if !(processStdin() || processArgs()) {
@@ -21,10 +23,7 @@ func main() {
 func processStdin() bool {
 	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
-		stdinScanner := bufio.NewScanner(os.Stdin)
-		for stdinScanner.Scan() {
-			fmt.Println(color.MagentaString(stdinScanner.Text()))
-		}
+		stdinMultiLineBubble()
 		return true
 	}
 	return false
@@ -40,14 +39,34 @@ func processArgs() bool {
 }
 
 func argsSpeak(args []string) {
-	maxLineLength := 38
 	text := strings.Join(args, " ")
 	lineLength := len(text)
 	if lineLength <= maxLineLength {
-		fmt.Printf(color.MagentaString("         %s\n", topLine(lineLength)))
-		fmt.Printf(color.CyanString("       %s  %s  %s\n", delimeters("only", 0), text, delimeters("only", 1)))
-		fmt.Printf(color.MagentaString("         %s\n", bottomLine(lineLength)))
+		oneLineBubble(text, lineLength)
 	}
+}
+
+func oneLineBubble(text string, lineLength int) {
+	fmt.Printf(color.MagentaString("         %s\n", topLine(lineLength)))
+	fmt.Printf(color.CyanString("       %s  %s  %s\n", delimeters("only", 0), text, delimeters("only", 1)))
+	fmt.Printf(color.MagentaString("         %s\n", bottomLine(lineLength)))
+}
+
+func stdinMultiLineBubble() {
+	var del0, del1 string
+	stdinScanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf(color.MagentaString("   %s\n", topLine(maxLineLength)))
+	for i := 0; stdinScanner.Scan(); i++ {
+		if i == 0 {
+			del0 = delimeters("first", 0)
+			del1 = delimeters("first", 1)
+		} else {
+			del0 = delimeters("middle", 0)
+			del1 = delimeters("middle", 1)
+		}
+		fmt.Printf(color.CyanString(" %s  %s %s\n", del0, pad(stdinScanner.Text()), del1))
+	}
+	fmt.Printf(color.MagentaString(" %s%s%s\n", delimeters("last", 0), bottomLine(maxLineLength), delimeters("last", 1)))
 }
 
 func delimeters(loc string, index int) string {
@@ -60,8 +79,8 @@ func delimeters(loc string, index int) string {
 	return dels[loc][index]
 }
 
-func pad(text string, length int) string {
-	padding := strings.Repeat(" ", length-(length+1))
+func pad(text string) string {
+	padding := strings.Repeat(" ", maxLineLength-len(text))
 	return text + padding
 }
 
