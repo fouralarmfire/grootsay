7add186d8ce80d85ffa161b57031e6a440fb5e8e
fouralarmfire <claudiaberesford@googlemail.com>
2016-10-05 23:44:31 +0100

    Do not need separate colour package

--- replica/i_am_groot.go
+++ replica/i_am_groot.go
@@ -1,17 +1,24 @@
 package replica
 
-import "github.com/fouralarmfire/grootsay/colours"
+import (
+	"fmt"
+	"math/rand"
+	"time"
 
-type Replica struct{}
+	"github.com/fatih/color"
+)
+
+type Replica struct {
+}
 
 func NewReplica() *Replica {
 	return &Replica{}
 }
 
 func (i *Replica) DefaultMessage() {
-	colours.PrintMagenta("        ____________")
-	colours.PrintCyan("      <  I AM GROOT  >")
-	colours.PrintMagenta("        ------------")
+	color.Magenta("        ____________")
+	color.Cyan("      <  I AM GROOT  >")
+	color.Magenta("        ------------")
 }
 
 func (i *Replica) Print() {
@@ -43,6 +50,26 @@ func (i *Replica) Print() {
 	}
 
 	for i, line := range lines {
-		colours.Randomize(line, i)
+		randomize(line, i)
 	}
+}
+
+func randomize(str string, ind int) {
+	color.Set(randomColour(ind))
+	defer color.Unset()
+	fmt.Println(str)
+}
+
+func randomColour(ind int) color.Attribute {
+	var colours = []color.Attribute{
+		color.FgRed,
+		color.FgGreen,
+		color.FgYellow,
+		color.FgBlue,
+		color.FgMagenta,
+		color.FgCyan,
+	}
+
+	rand.Seed(int64(ind-(-1)) * time.Now().Unix())
+	return colours[rand.Int()%len(colours)]
 }
